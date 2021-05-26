8d065c43096e7b4e47594d2b7e86f6b8c20c84f2
Balazs Kocsis <balazs_kocsis@laurastar.com>
2021-05-26 13:32:55 +0200

    SharePointListOperations v1.0.0: initial commit

---
+++ SharePointListOperations/LSSqlOperations.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SharePointListOperations
+{
+    public class LSSqlOperations
+    {
+        //Connect to the SQL server
+        public static bool ConnectToSqlServer(SqlConnection conn, string conn_string)
+        {
+            bool is_connected = false;
+
+            try
+            {
+                conn.Open();
+                is_connected = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nem sikerült csatalkozni az adatbázishoz" + "\n\r" + ex);
+            }
+
+            return is_connected;
+        }
+
+        //Query from the SQL server with datareader, returns with the value of the "result field"
+        public static string SqlQuery(SqlConnection conn, string query, bool date_based, string param1, string paramvalue1, string param2, string paramvalue2, string result_field)
+        {
+            string result = "";
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = query;
+
+            if (date_based)
+            {
+                command.Parameters.AddWithValue("@start_date", DateTime.Parse(DateTime.Now.ToShortDateString().ToString() + " 0:00:00"));
+                command.Parameters.AddWithValue("@end_date", DateTime.Parse(DateTime.Now.ToShortDateString().ToString() + " 23:59:00"));
+            }
+
+            if (param1 != null && paramvalue1 != null)
+            {
+                command.Parameters.AddWithValue(param1, paramvalue1);
+            }
+
+            if (param2 != null && paramvalue2 != null)
+            {
+                command.Parameters.AddWithValue(param2, paramvalue2);
+            }
+
+            SqlDataReader reader = (null);
+
+            try
+            {
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    result = (reader[result_field].ToString());
+                    //Console.WriteLine(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("A lekérdezés futtatása nem sikerült.\r\n" + ex);
+            }
+
+            if (reader != null)
+            {
+                reader.Close();
+            }
+            command.Cancel();
+
+            return result;
+        }
+
+        //SQL Query to Datatable
+        public static DataTable SQLDataTableQuery(SqlConnection conn, string query, bool date_based, string param1, string paramvalue1, string param2, string paramvalue2, string result_field)
+        {
+            DataTable d_table = new DataTable();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = query;
+
+            if (date_based)
+            {
+                command.Parameters.AddWithValue("@start_date", DateTime.Parse(DateTime.Now.ToShortDateString().ToString() + " 0:00:00"));
+                command.Parameters.AddWithValue("@end_date", DateTime.Parse(DateTime.Now.ToShortDateString().ToString() + " 23:59:00"));
+            }
+
+            if (param1 != null && paramvalue1 != null)
+            {
+                command.Parameters.AddWithValue(param1, paramvalue1);
+            }
+
+            if (param2 != null && paramvalue2 != null)
+            {
+                command.Parameters.AddWithValue(param2, paramvalue2);
+            }
+
+            SqlDataAdapter d_adapter = new SqlDataAdapter();
+
+            return d_table;
+        }
+    }
+}
--- SharePointListOperations/Program.cs
+++ SharePointListOperations/Program.cs
@@ -1,15 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace SharePointListOperations
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            string amplio_connstring = ConfigurationManager.AppSettings["amplio_connectionstring"] + "BalKoc%22;";
+            string mes_connstring = ConfigurationManager.AppSettings["mod_connectionstring"] + "MoMe19l.";
+
+            string testedQtyQuery = ConfigurationManager.AppSettings["tested_qty_query"];
+            string plannedQtyQuery = ConfigurationManager.AppSettings["planned_qty_query"];
+            string clockQuery = ConfigurationManager.AppSettings["clock_query"];
+
+            bool dateBasedQuery = true;
+            string testedQty = "";
+            string plannedQty = "";
+            SqlConnection amplio_conn = new SqlConnection(amplio_connstring);
+            SqlConnection mes_conn = new SqlConnection(mes_connstring);
+
+            bool amplio_is_connected = LSSqlOperations.ConnectToSqlServer(amplio_conn, amplio_connstring);
+            bool mes_is_connected = LSSqlOperations.ConnectToSqlServer(mes_conn, mes_connstring);
+
+            if (amplio_is_connected)
+            {
+                testedQty = LSSqlOperations.SqlQuery(amplio_conn, testedQtyQuery, dateBasedQuery, null, null, null, null, "tested_count");
+            }
+
+            if (mes_is_connected)
+            {
+                plannedQty = LSSqlOperations.SqlQuery(mes_conn, plannedQtyQuery, false, null, null, null, null, "other");
+            }
+
+            
+
+            string siteCollectionUrl = "https://laurastar.sharepoint.com/sites/MonitoringDatas";
+            string userName = "bakocsis@laurastar.com";
+            string password = "BalKoc%42";
+            Program obj = new Program();
+
+            ClientContext ctx = new ClientContext(siteCollectionUrl);
+
+            try
+            {
+                ctx = obj.ConnectToSharePointOnline(siteCollectionUrl, userName, password, ctx);
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                Console.WriteLine(msg);
+
+            }
+
+            SP.List oList = ctx.Web.Lists.GetByTitle("ProductionData");
+            ListItem oListItem = oList.GetItemById(1);
+
+            oListItem["Tested"] = Convert.ToInt32(testedQty);
+            oListItem["Plan"] = Convert.ToInt32(plannedQty);
+            oListItem["LastUpdate"] = DateTime.Parse(DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss"));
+
+            oListItem.Update();
+
+            ctx.ExecuteQuery();
         }
+        public ClientContext ConnectToSharePointOnline(string siteCollUrl, string userName, string password, ClientContext ctx_local)
+        {
+
+            //Namespace: It belongs to Microsoft.SharePoint.Client
+            //ClientContext ctx_local = new ClientContext(siteCollUrl);
+
+            // Namespace: It belongs to System.Security
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            ctx_local.Credentials = new SharePointOnlineCredentials(userName, secureString);
+
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            Site mySite = ctx_local.Site;
+
+            ctx_local.Load(mySite);
+            ctx_local.ExecuteQuery();
+
+            Console.WriteLine(mySite.Url.ToString());
+
+            return ctx_local;
+        }
+
+        
+
+
     }
 }
+
+
